99a27164a1f59e43507c2287c52ea69a5bd5ba64
Joe Adkisson <adkissonj@carleton.edu>
2015-05-10 12:30:55 -0500

    Adding a unit testing class for the concrete objects (State, Committee, Bill)

---
+++ ObjectTests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from Bill import Bill
+from Senator import Senator
+from Committee import Committee
+
+class Test
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
