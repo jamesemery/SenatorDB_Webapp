2a7cc6ec97f35886af76bfc20403acd857db366b
jamie <emeryj@carleton.edu>
2015-05-10 18:27:51 -0500

    More fixing things

--- DataSource.py
+++ DataSource.py
@@ -37,7 +37,7 @@ class DataSource:
     def getBill(self, bill_id):
         try:
             cursor = db_connection.cursor()
-            cursor.execute('SELECT id, session, roll, vote_date, type, question FROM bills WHERE id = (%s);',
+            print cursor.mogrify('SELECT id, session, roll, vote_date, type, question FROM bills WHERE id = (%s);',
                 (bill_id, ))
             bills = []
             for row in cursor:
