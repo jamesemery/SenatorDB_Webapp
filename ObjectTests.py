258cbb95a25cbf356feed1221eb97637235fb838
Joe Adkisson <adkissonj@carleton.edu>
2015-05-10 16:52:24 -0500

    Tests for Bill Constructor

--- ObjectTests.py
+++ ObjectTests.py
@@ -9,10 +9,43 @@ from Committee import Committee
 
 #Unit tests for the three concrete classes - 
 #Bill, Senator, and Committee.
-class ObjectTests(unittest.testCase):
+class ObjectTests(unittest.TestCase):
 
-	def testBillConstructor(self):
-		#do stuff
+	def testBillConstructorShort(self):
+		params = [100, 113, 2, "3/5/2012", "bill", "Question"]
+		bill = Bill(params)
+		self.assertEqual(bill.getId(), 100)
+		self.assertEqual(bill.getSession(), 113)
+		self.assertEqual(bill.getRoll(), 2)
+		self.assertEqual(bill.getVoteDate(), "3/5/2012")
+		self.assertEqual(bill.getType(), "bill")
+		self.assertEqual(bill.getQuestion(), "Question")
+		#we don't have a simple getter for these, so we're grabbing them directly
+		self.assertEqual(bill.yea_votes, [])
+		self.assertEqual(bill.nay_votes, [])
+		self.assertEqual(bill.abstaining, [])
+		self.assertEqual(bill.absent, [])
+
+	def testBillConstructorLong(self):
+		#The last 4 parameters should be lists of Senator objects, but b/c of
+		#Python's typing it doesn't matter for testing purposes. We're just
+		#making them strings.
+		params = [100, 113, 2, "3/5/2012", "bill", "Question", 
+					["Rich White, Richard Whiter"],
+					["John Brown"],
+					[],
+					["George Washington"]]
+		bill = Bill(params)
+		self.assertEqual(bill.getId(), 100)
+		self.assertEqual(bill.getSession(), 113)
+		self.assertEqual(bill.getRoll(), 2)
+		self.assertEqual(bill.getVoteDate(), "3/5/2012")
+		self.assertEqual(bill.getType(), "bill")
+		self.assertEqual(bill.getQuestion(), "Question")
+		self.assertEqual(bill.yea_votes, ["Rich White, Richard Whiter"])
+		self.assertEqual(bill.nay_votes, ["John Brown"])
+		self.assertEqual(bill.abstaining, [])
+		self.assertEqual(bill.absent, ["George Washington"])
 
 	def testCommitteeConstructorShort(self):
 		params = [100, "Committee to Discuss Commit Messages in Git",
@@ -37,22 +70,23 @@ class ObjectTests(unittest.testCase):
 		self.assertEqual(com.getSession(), 113)
 		self.assertEqual(com.getSenators(), ["sen1", "sen2", "sen3"])
 
-	def testSenatorConstructor(self):
+	#def testSenatorConstructor(self):
 		#do stuff
 
 	def testCommitteeLinkGenerator(self):
-		htmlLink = ""
+		htmlLink = "thisWillFail"
 		params = [100, "Committee to Discuss Commit Messages in Git",
 			"super-committee", 113, ["sen1", "sen2", "sen3"]]
 		com = Committee(params)
 		self.assertEqual(com.getCommitteeLink(), htmlLink)
 
 	def testBillLinkGenerator(self):
-		htmlLink = ""
+		htmlLink = "thisWillFail"
 
 	def testSenatorLinkGenerator(self):
+		htmlLink = "thisWillFail"
 
-	def testBillVoteTally(self):
+	#def testBillVoteTally(self):
 		#because Python doesn't care about typing, we're just going to pass in
 		#strings instead of Senator objects for testing purposes. The method
 		#should work the same either way.
