e1be96a8a3709d7752b6421e2e071e78f09fda67
Joe Adkisson <adkissonj@carleton.edu>
2015-05-11 03:17:16 -0500

    trying another thing

--- Senator.py
+++ Senator.py
@@ -32,7 +32,8 @@ class Senator:
 		return htmlLink
 
 	def getSenatorLink(self):
-		htmlLink = '<a href = "http://thacker.mathcs.carleton.edu/cs257/emeryj/index.py?page_type=senator&senator="' + str(self.getId()) + ">" + self.getName() + "</a>"
+		str_id = str(self.getId())
+		htmlLink = '<a href = "http://thacker.mathcs.carleton.edu/cs257/emeryj/index.py?page_type=senator&senator="' + str_id + ">" + self.getName() + "</a>"
 		return htmlLink
 
 	def getFirst(self):
