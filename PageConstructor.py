d88d3220d00f87cf4cf0df32decba6c8e6b93920
Joe Adkisson <adkissonj@carleton.edu>
2015-05-11 04:12:08 -0500

    fixed abbreviations for real

--- PageConstructor.py
+++ PageConstructor.py
@@ -27,10 +27,10 @@ class PageConstructor:
 		'West Virginia', 'Wisconsin', 'Wyoming']
 
 	STATE_ABBREVIATION_LIST = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT',
-		, 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME'
-		, 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM'
-		, 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX'
-		, 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+		'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',
+		'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM',
+		'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+		'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
 	#Constructor that builds a PageConstructor object and an empty string for
 	#the page.
